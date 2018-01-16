dbadd926164d553ab1e6ab0fd642a58b3197c95f
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-15 17:29:21 -0800

    Update component to use a model and create a new Task using constructor.

--- app/app.component.ts
+++ app/app.component.ts
@@ -23,8 +23,13 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   //In Angular, when a variable in a component's class declaration references
-  //another variable in the class, it must be prefaced with the this keyword.
-  firstTask = {
-    description: "✏️Finish weekend Angular homework for Epicodus Course"
-  }
+  //another variable in the class, it must be prefaced with the this. keyword.
+  firstTask: Task = new Task("✏️Finish weekend Angular homework for Epicodus Course");
 }
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string){ }
+}
+
+//Because we've exported a Task class, Task is now a valid data type for variables!
