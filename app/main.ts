c55f10937c94fa20c36ac2aab59677bfced2211f
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-15 16:37:29 -0800

    Create entry point file.

---
+++ app/main.ts
@@ -0,0 +1,8 @@
+//entry point file 🚪
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+
+platform.bootstrapModule(AppModule);
